229cb0554500a24947eb635c04e300d600f017f2
Thomas Siqueira Pereira <thomassiqueira427@gmail.com>
2024-04-06 22:12:34 -0300

    new branch commit, first test with design

---
+++ lib/up_app_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UpAppBar extends StatelessWidget {
+  const UpAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('tittle test'),
+      ),
+    );
+  }
+}
