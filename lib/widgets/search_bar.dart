8874d58170caa42ba3c82be7369ba82ac320b118
Raineri-CS <Raineri-CS@github.com>
2024-04-14 17:40:44 -0300

    Initial commit for the results page

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
 class SearchBox extends StatelessWidget {
-  const SearchBox({super.key});
+  SearchBox({super.key});
+
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Stack(
         children: [
-          SearchBar(
-            hintText: "Pesquisar",
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              hintText: "Pesquisar",
+            ),
+            onSubmitted: (value) {
+              // Aqui você pode usar o Navigator para mudar a página
+              // Use o valor para a pesquisa
+            },
           ),
           Padding(
             padding: const EdgeInsets.all(3.0),
             child: Align(
               child: GestureDetector(
                 onTap: () {
-                  // TODO pesquisa aqui!
+                  print(_controller.text);
+                  // Aqui você pode usar o Navigator para mudar a página
+                  // Use _controller.text para obter o texto da caixa de pesquisa
                 },
                 child: Icon(
                   Icons.search,
